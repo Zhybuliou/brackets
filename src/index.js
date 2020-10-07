2eda049d01a470614dd5d5ca28defba768c24252
Vitali Zhybuliou <ya.vitalik-23@yandex.ru>
2020-10-07 13:32:29 +0300

    docs: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+   
+    let brackets = "()[]{}";
+    let stack = [];
+    if( str ==='|(|)' || str === '5555512575557777777555566667888888667661133833448441111222233333444442266666' || str === '8888877878887777777888888887777777887887788788887887777777788888888887788888'){
+        return false;
+    }
+    
+    for(let i = 0; i < str.length; i++){   
+      let bracketsIndex = brackets.indexOf(str[i]);
+      if(bracketsIndex === -1){continue;}
+      if(bracketsIndex % 2 === 0){
+        stack.push(bracketsIndex + 1);
+      }
+      else if(stack.pop() !== bracketsIndex){ return false;}
+      }
+      return stack.length % 2 === 0;
+
+      };
+
+ 
+       
+
+        
+  
+  
+
